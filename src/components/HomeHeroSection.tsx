6813071589a6432fea5d1effbf75d1ed90011e47
Include caleb.fun <70612830+oasisMystre@users.noreply.github.com>
2024-01-25 12:43:04 -0800

    Update HomeHeroSection.tsx

--- src/components/HomeHeroSection.tsx
+++ src/components/HomeHeroSection.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Image from "next/image";
 import { IlDecentralizeBank } from "@/assets";
 
@@ -15,7 +16,9 @@ export default function HomeHeroSection() {
           Solocker helps you keep transparency and secure liquidity lock with
           user friendly interface for your projects.
         </p>
-        <button className="self-start btn btn-primary">Learn More</button>
+        <Link 
+          href="/token-lock"
+          className="self-start btn btn-primary">LP Lock</Link>
       </div>
       <div className="flex items-center justify-center md:flex-1 md:w-sm md:h-sm md:absolute md:right-0 md:top-0">
         <Image

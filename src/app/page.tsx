b0b6e624da44a858ff08e265650b0c2513374835
Include caleb.fun <70612830+oasisMystre@users.noreply.github.com>
2024-01-25 12:59:44 -0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { homeSteps } from "@/data";
+import LayoutFooter from "@/components/LayoutFooter";
 import HomeHeroSection from "@/components/HomeHeroSection";
 import HomeStepSection from "@/components/HomeStepSection";
 import HomeFeatureSection from "@/components/HomeFeatureSection";
@@ -8,11 +9,14 @@ import HomeTestimonySection from "@/components/HomeTestimonySection";
 
 export default function HomePage() {
   return (
-    <div className="flex flex-col space-y-16 py-16">
-      <HomeHeroSection />
-      <HomeFeatureSection />
-      <HomeStepSection />
-      <HomeTestimonySection />
-    </div>
+    <>
+      <div className="flex flex-col space-y-16 py-16">
+        <HomeHeroSection />
+        <HomeFeatureSection />
+        <HomeStepSection />
+        <HomeTestimonySection />
+      </div>
+      <LayoutFooter />
+    </>
   );
 }

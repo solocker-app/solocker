9ab5387701004bca0a5c06230350083535c69562
Include caleb.fun <70612830+oasisMystre@users.noreply.github.com>
2024-01-27 15:17:31 -0800

    update src/store/tokenLockSlice.ts

---
+++ src/store/tokenLockSlice.ts
@@ -0,0 +1,38 @@
+import { createSlice, createEntityAdapter, createAsyncThunk } from "@reduxjs/toolkit";
+
+import StreamFlow from  "@/lib/streamflow";
+
+type LoadingState = "idle" | "pending" | "error" | "success";
+type TokenLock = Awaited<StreamFlow["getLockedTokens"]>[number];
+
+export const getLockedTokens = createAsyncThunk(
+  "tokenLock/getLockedTokens", 
+  (streamflow: StreamFlow) => streamflow.getLockedTokens()
+);
+
+export const tokenLockAdapter = createEntityAdapter<TokenLock>({
+  selectId: ([id]) => id,
+});
+
+export const tokenLockSlice = createSlice({
+  name: "tokenLock",
+  initialState: tokenLockAdapter.getInitialState({
+    loadingState: "idle" as LoadingState,
+  }),
+  reducers: {},
+  extraReducers(builder){
+    builder.addCase(getLockedTokens.pending, (state) => {
+      state.loadingState = "pending";
+    })
+    .addCase(getLockedTokens.rejected, (state) => {
+      state.loadingState = "error";
+    })
+    .addCase(getLockedTokens.fulfilled, (state, data) => {
+      state.loadingState = "success";
+      tokenLockAdapter.setMany(state, data);
+    })
+  }
+});
+
+export const tokenLockReducer = tokenLockSlice.reducer;
+export const tokenLockSelector = tokenLockAdapter.getSelector();

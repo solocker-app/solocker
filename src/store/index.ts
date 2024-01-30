f2978201d00a889022d138097412702f93eeeab8
Include caleb.fun <70612830+oasisMystre@users.noreply.github.com>
2024-01-30 00:54:04 -0800

    Update index.ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import { tokenLockReducer } from "./slices/tokenLock";
-import { digitalAssetReducer } from "./slices/digitalAsset";
 import { raydiumLpInfoReducer } from "./slices/raydiumLpInfo";
 import { streamflowReducer } from "./slices/streamflow";
 
 export const store = configureStore({
   reducer: {
     tokenLock: tokenLockReducer,
-    digitalAsset: digitalAssetReducer,
     raydiumLpInfo: raydiumLpInfoReducer,
     streamFlow: streamflowReducer,
   },

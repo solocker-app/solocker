d340cbf269826116df76bcb397670cbf30f3134f
Include caleb.fun <70612830+oasisMystre@users.noreply.github.com>
2024-02-02 13:29:37 +0100

    Update index.ts

--- src/lib/streamflow/index.ts
+++ src/lib/streamflow/index.ts
@@ -3,7 +3,7 @@ import { StreamflowSolana, getBN, Types } from "@streamflow/stream";
 
 import type { Config } from "../models/config.model";
 
-import { percentFee } from "./instructions";
+import { percentFee,  createFeeInstruction } from "./instructions";
 import type { BaseRepository } from "..";
 import { InjectBaseRepository } from "../injector";
 

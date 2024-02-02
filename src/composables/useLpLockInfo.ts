ea1518ca71cbd13e4f4fd959c77d7dcd25ef8db1
Oguntunde Caleb Fiyinfoluwa <oasis.mystre@gmail.com>
2024-02-02 08:31:20 +0100

    ...

--- src/composables/useLpLockInfo.ts
+++ src/composables/useLpLockInfo.ts
@@ -1,30 +1,29 @@
-import { useWallet } from "@solana/wallet-adapter-react"; 
- import { Types } from "@streamflow/stream";
+import { useEffect } from "react";
 
-import { 
-   getLiquidityPoolInfo, 
-   raydiumLpInfoSelector, 
- } from "@/store/slices/raydiumLpInfo"; 
- import { useAppDispatch, useAppSelector } from "@/store/hooks"; 
- 
- import { LpInfo } from "@/lib/api/models/raydium.model";
- 
-export default function useLpLockInfo(stream: Types.Stream){
-  const { publicKey } = useWallet(); 
-  const dispatch = useAppDispatch(); 
-  const state = useAppSelector((state) => state.raydiumLpInfo); 
-  
-  const lpInfo = raydiumLpInfoSelector.selectById(state, stream.mint); 
-  
-  useEffect(() => { 
-    getLiquidityPoolInfo(state, dispatch, { 
-      wallet: publicKey.toBase58(), 
-      mint: stream.mint, 
-   }); 
-   }, [state, dispatch, wallet]);
-   
-   
-   return {
-     lpInfo,
-   }
+import { useWallet } from "@solana/wallet-adapter-react";
+import { Types } from "@streamflow/stream";
+
+import {
+  getLiquidityPoolInfo,
+  raydiumLpInfoSelector,
+} from "@/store/slices/raydiumLpInfo";
+import { useAppDispatch, useAppSelector } from "@/store/hooks";
+
+export function useLpLockInfo(stream: Types.Stream) {
+  const { publicKey } = useWallet();
+  const dispatch = useAppDispatch();
+  const state = useAppSelector((state) => state.raydiumLpInfo);
+
+  const lpInfo = raydiumLpInfoSelector.selectById(state, stream.mint);
+
+  useEffect(() => {
+    getLiquidityPoolInfo(state, dispatch, {
+      wallet: publicKey.toBase58(),
+      mint: stream.mint,
+    });
+  }, [state, dispatch, publicKey]);
+
+  return {
+    lpInfo,
+  };
 }

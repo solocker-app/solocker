568cfaaafee7f91f183b6648deb178f444de2a6e
Include caleb.fun <70612830+oasisMystre@users.noreply.github.com>
2024-02-07 13:18:07 +0100

    Update instructions.ts

--- src/lib/streamflow/instructions.ts
+++ src/lib/streamflow/instructions.ts
@@ -3,33 +3,12 @@ import { SystemProgram, PublicKey, LAMPORTS_PER_SOL  } from "@solana/web3.js";
 import { createTransferInstruction } from "@solana/spl-token";
 
 import type { BaseRepository } from "..";
-import { getOrCreateAssociatedTokenAccount } from "../utils/web3";
+
+export const marketingWallet = new PublicKey(process.env.NEXT_PUBLIC_MARKETING_WALLET);
 
 export async function createFeeInstructions(repository: BaseRepository, fee: BN, mintAddress: string){
-  const { connection, wallet, signTransaction } = repository;
-  
   const mint = new PublicKey(mintAddress);
-  const marketingWallet = new PublicKey(process.env.NEXT_PUBLIC_MARKETING_WALLET);
-  
- const sourceAccount = await getOrCreateAssociatedTokenAccount(
-    connection,
-    wallet.publicKey,
-    mint,
-    wallet.publicKey,
-    signTransaction,
-  );
-  
-  console.log(sourceAccount)
-  
-  const destinationAccount = await getOrCreateAssociatedTokenAccount(
-    connection,
-    wallet.publicKey,
-    mint,
-    marketingWallet,
-    signTransaction,
-  );
-  
-  console.log(destinationAccount);
+  const { connection, wallet, signTransaction } = repository;
   
   return [
     SystemProgram.transfer({
@@ -37,11 +16,5 @@ export async function createFeeInstructions(repository: BaseRepository, fee: BN,
       toPubkey: marketingWallet,
       lamports: 2 * LAMPORTS_PER_SOL,
     }),
-    createTransferInstruction(
-      sourceAccount.address,
-      destinationAccount.address,
-      wallet.publicKey,
-      fee,
-    ), 
   ];
 }

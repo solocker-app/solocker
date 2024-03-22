1a8d15cb0366d5c7b272eae920494bbbb5e25320
Include caleb.fun <70612830+oasisMystre@users.noreply.github.com>
2024-03-23 00:46:59 +0100

    Update raydium.ts

--- src/lib/api/raydium.ts
+++ src/lib/api/raydium.ts
@@ -2,12 +2,9 @@ import { AxiosResponse } from "axios";
 import { InjectAxios } from "./injector";
 import type { LpInfo } from "./models/raydium.model";
 
-type Cache = Map<string, LpInfo>;
-
 export default class RaydiumApi extends InjectAxios {
   path: string = "/raydium/lp-infos/";
-  static cache: Map<string, AxiosResponse> = new Map();
-
+  
   async fetchLpInfos(wallet: string) {
     return this.axios.post<LpInfo[]>(this.path, {
       wallet,
@@ -15,8 +12,6 @@ export default class RaydiumApi extends InjectAxios {
   }
 
   async fetchLpInfo(mint: string, wallet: string = null) {
-    if (RaydiumApi.cache.has(mint)) return RaydiumApi.cache.get(mint);
-
     return this.axios.post<LpInfo>(this.buildPath(mint), {
       wallet,
     });

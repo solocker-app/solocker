4df7549e2acd6344822ad9b1b5e1276a6d774768
Include caleb.fun <70612830+oasisMystre@users.noreply.github.com>
2024-03-23 02:25:57 +0100

    Update InputDate.tsx

--- src/components/widgets/InputDate.tsx
+++ src/components/widgets/InputDate.tsx
@@ -39,7 +39,7 @@ export default function InputDate({ name, value }: InputDateProps) {
             input={false}
             open={true}
             isValidDate={(currentDate: ReturnType<typeof moment>) =>
-              currentDate.isSameOrAfter(moment.now())
+              moment.now().isSameOrAfter(currentDate)
             }
             onChange={(value) => {
               if (typeof value !== "string") setFieldValue(name, value.unix());

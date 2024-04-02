3857bc9b5dafae5f4c5d71307bb59cba443dd1f1
Include caleb.fun <70612830+oasisMystre@users.noreply.github.com>
2024-04-02 12:36:51 +0100

    feat: update lpInfo to fetch using get instead of post

--- src/lib/api/raydium.ts
+++ src/lib/api/raydium.ts
@@ -1,19 +1,22 @@
-import { AxiosResponse } from "axios";
 import { InjectAxios } from "./injector";
 import type { LpInfo } from "./models/raydium.model";
 
 export default class RaydiumApi extends InjectAxios {
   path: string = "/raydium/lp-infos/";
-  
+
   async fetchLpInfos(wallet: string) {
-    return this.axios.post<LpInfo[]>(this.path, {
-      wallet,
-    });
+    return this.axios.get<LpInfo[]>(
+      this.buildQuery(this.path, {
+        wallet,
+      }),
+    );
   }
 
   async fetchLpInfo(mint: string, wallet: string = null) {
-    return this.axios.post<LpInfo>(this.buildPath(mint), {
-      wallet,
-    });
+    return this.axios.get<LpInfo>(
+      this.buildQuery(this.buildPath(mint), {
+        wallet,
+      }),
+    );
   }
 }
